7586dfce4d532e906b1bbaee8d5bf10f76ec7ed8
Lara-van-Tonder <125471915+Lara-van-Tonder@users.noreply.github.com>
2023-07-26 15:33:40 +0200

    Complete Week 3 Class Exercise
    Add Browse Page.
    Add Browse Page Content.
    Add Card Animation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,28 @@
 // When the document loads
-$(document).ready{function{}{
+$(document).ready(function(){
 
     console.log("Hello");
 
     // When the document loads, animate the hero image upwards
-    $(".hero-image").animate({top: '-=100px'});
+    $("#hero-image").animate({top: '-=100px'});
 
-}};
+
+    // --------------------------------------------------
+    // Browse
+
+    // Hide all description text from the plant cards
+    $("#descriptionText").hide();
+
+});
+
+// When a plant card is clicked
+$(".card").click(function(){
+
+    // Toggle the price and description text
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // Resize the image to fit the additional content
+    $(".card-img-top").toggleClass("small");
+
+});
