d551f8f2d3853d9bf15b5b77765503bfd55907af
Lara-van-Tonder <125471915+Lara-van-Tonder@users.noreply.github.com>
2023-08-16 13:10:01 +0200

    Week 5 Classwork Complete
    Week 5 Classwork caught up and complete.

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,57 @@
+// ------------------------------------------------------------------------
 // Plants Array
-// -----------------------------------------
+// ------------------------------------------------------------------------
 
-const plants = [
-    {
-      "name": "Ficus Tree",
-      "price": 350,
-      "description": "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space.",
-      "image": "plant1.png"
-    },
-    {
-      "name": "White Sprite Succulent",
-      "price": 200,
-      "description": "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
-      "image": "plant2.png"
-    },
-    {
-      "name": "Snake Plant",
-      "price": 400,
-      "description": "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
-      "image": "plant3.png"
-    },
-    {
-      "name": "Parlour Palm",
-      "price": 350,
-      "description": "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
-      "image": "plant4.png"
-    },
-    {
-      "name": "Japanese Maple",
-      "price": 1200,
-      "description": "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
-      "image": "plant5.png"
-    }
-  ];
+const arrPlants = [
+  {
+    name: "Ficus Tree",
+    price: 350,
+    description: "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space.",
+    image: "plant1.png",
+    lightAmount: "low",
+    addedDate: "2023-03-25",
+    origin: "Texas"
+  },
+  {
+    name: "White Sprite Succulent",
+    price: 200,
+    description: "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
+    image: "plant2.png",
+    lightAmount: "bright",
+    addedDate: "2023-05-01",
+    origin: "China"
+  },
+  {
+    name: "Snake Plant",
+    price: 400,
+    description: "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
+    image: "plant3.png",
+    lightAmount: "low",
+    addedDate: "2023-07-04",
+    origin: "London"
+  },
+  {
+    name: "Parlour Palm",
+    price: 350,
+    description: "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
+    image: "plant4.png",
+    lightAmount: "low",
+    addedDate: "2023-04-29",
+    origin: "Pretoria"
+  },
+  {
+    name: "Japanese Maple",
+    price: 1200,
+    description: "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
+    image: "plant5.png",
+    lightAmount: "bright",
+    addedDate: "2023-05-10",
+    origin: "Japan"
+  },
+];
+
+let appliedFilter = "";
+let appliedSort = "date added";
 
 
 
@@ -54,7 +73,7 @@ $(document).ready(function(){
     // -----------------------------------------
     // Browse Page
 
-    loadPlants();
+    filterSortPlants();
 
 }); 
 
@@ -65,30 +84,113 @@ $(document).ready(function(){
 // Load all plants
 // -----------------------------------------
 
-function loadPlants() {
+function loadPlants(plantsToShow) {
 
-    // Load and display all the plants
-    for (let i = 0; i < plants.length; i++) {
-        const plant = plants[i];
-        
-        console.log(plant);
+  // Clear all elements inside the plants cards container
 
-        // 1: Select the plants container and add the plant card to it (current array plant)
-        $("#plantsContainer").append($("#plantCardTemplate").html());
+  $("#plantsContainer").empty();
 
-        // 2: Create a variable that contains the most recently added plant card
-        let currentChild = $("#plantsContainer").children().eq(i+1);
+  // Loop though plants
 
-        // 3: Set the content for the current plant card from the plant array
-        $(currentChild).find(".card-img-top").attr('src','assets/' + plant.image);
-        $(currentChild).find("#nameText").text(plant.name);
-        $(currentChild).find("#priceText").text('R' + plant.price);
-        $(currentChild).find("#descriptionText").text(plant.description);
+  for (let i = 0; i < plantsToShow.length; i++) {
+    const plant = plantsToShow[i];
+    
+    console.log(plant.name);
 
-        // 4: Hide the description text from the current plant card
-        $(currentChild).find("#descriptionText").hide();
+    $.ajax({
+      type: "GET",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + plant.origin + "&appid=0c8a911e5c7f8e5a03991afe2075de21",
+      success: function (data) {
+        tempData = data;
+        console.log(tempData);
+      },
+    }).done(function () {
+      //// Set Temperature
+      //// Will give the result with a higher value
 
-    }
+      // $(currentChild).find("#nameText").text(tempData.main.temp);
+      
+      $(currentChild).find("#originTemp").text("Origin Temp: " + Math.round(tempData.main.temp- 273) + "°C");
+     
+    
+    });
+
+    // 1: Select the plants container add the plant card to it
+    $("#plantsContainer").append($("#plantCardTemplate").html());
+
+    // 2: Create a variable that contains the most recently added plant card
+    let currentChild = $("#plantsContainer").children().eq(i);
+
+    // 3: Set the content for the current plant card from the plant array
+    $(currentChild).find("#nameText").text(plant.name);
+    $(currentChild).find("#priceText").text("R" + plant.price);
+    $(currentChild).find("#descriptionText").text(plant.description);
+    $(currentChild).find(".card-img-top").attr('src','assets/' + plant.image);
+
+    // 4: Hide the description text from the curent card
+    $(currentChild).find("#descriptionText").hide();
+    $(currentChild).find("#originTemp").hide();
+
+  };
+
+};
+
+// ------------------------------------------------------------------------
+// When a filter or sort option is clicked
+// ------------------------------------------------------------------------
+
+$("input[name='filterRadio']").click(function(){
+  appliedFilter = $(this).attr('value');
+
+  filterSortPlants();
+});
+
+$("input[name='sortRadio']").click(function(){
+  appliedSort = $(this).attr('value');
+
+  filterSortPlants();
+});
+
+function filterSortPlants() {
+  
+  let filteredSortedArrPlants = [];
+
+  console.log(appliedFilter);
+  console.log(appliedSort);
+
+  // Filter Plants
+
+  if (appliedFilter) {
+    filteredSortedArrPlants = arrPlants.filter(plant => plant.lightAmount == appliedFilter);
+  } else {
+    filteredSortedArrPlants = arrPlants;
+  }
+
+  // Sort Plants
+
+  if (appliedSort == "low to high") {
+
+    // Sort plants from the lowest to highest price
+    filteredSortedArrPlants = filteredSortedArrPlants.sort((a, b) => {
+      return a.price - b.price;
+    });
+
+  } else if (appliedSort == "date added") {
+
+    // Sort plants from the newest to oldest
+    filteredSortedArrPlants = filteredSortedArrPlants.sort((a, b) => {
+      let da = new Date(a.addedDate);
+      let db = new Date(b.addedDate);
+    
+      return db - da;
+    });
+
+  }
+
+  console.log(filteredSortedArrPlants)
+
+  loadPlants(filteredSortedArrPlants);
+
 }
 
 
